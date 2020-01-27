a0e74155a4757b0d5c44c03d19f50c13aba729cc
Noah Liam Cornett <cornettn@xinu02.cs.purdue.edu>
2020-01-27 17:21:41 -0500

    Changed welcome message slighlty

--- system/mymotd.c
+++ system/mymotd.c
@@ -3,5 +3,6 @@
 void mymotd(void) {
   char *name = "Noah Cornett";
   char *username = "cornettn";
-  kprintf("\nHello! My name is %s and my username is %s\n", name, username);
+  kprintf("\nHello! My name is %s and I am the developer of this version of"
+     " xinu! My username is %s.", name, username);
 }
