dc0848f3716fd048d3141833fc520bfc36cfde92
Noah Liam Cornett <cornettn@xinu01.cs.purdue.edu>
2020-02-06 15:22:24 -0500

    Turned off macros

--- include/process.h
+++ include/process.h
@@ -4,8 +4,8 @@
  * Macros for print statements -- 0 off, 1 on
  */
 
-#define XINUTEST 1
-#define XINUDEBUG 1
+#define XINUTEST 0
+#define XINUDEBUG 0
 
 
 /* Maximum number of processes in the system */
