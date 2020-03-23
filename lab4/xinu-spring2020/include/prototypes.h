0cc1a2af78d8257fad258133d0f296ef7293f21c
Noah Liam Cornett <cornettn@xinu01.cs.purdue.edu>
2020-03-23 16:54:28 -0400

    Added bsend to syscall prototypes

--- lab4/xinu-spring2020/include/prototypes.h
+++ lab4/xinu-spring2020/include/prototypes.h
@@ -613,6 +613,9 @@ extern	void	xdone(void);
 /* in file yield.c */
 extern	syscall	yield(void);
 
+/* in file bsend.c */
+extern  syscall bsend(pid32, umsg32);
+
 /* NETWORK BYTE ORDER CONVERSION NOT NEEDED ON A BIG-ENDIAN COMPUTER */
 #define	htons(x)  ((0xff & ((x)>>8)) | ((0xff & (x)) << 8))
 #define	htonl(x)  ((((x)>>24) & 0x000000ff) | (((x)>> 8) & 0x0000ff00) | \
