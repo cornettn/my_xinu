9d25ffb7d6de0cd1e551cecc57c3eeab5749e82b
Noah Liam Cornett <cornettn@xinu01.cs.purdue.edu>
2020-03-23 16:51:32 -0400

    Added bsend and changes to queue.h

--- lab4/xinu-spring2020/include/queue.h
+++ lab4/xinu-spring2020/include/queue.h
@@ -3,9 +3,16 @@
 /* Queue structure declarations, constants, and inline functions	*/
 
 /* Default # of queue entries: 1 per process plus 2 for ready list plus	*/
-/*			2 for sleep list plus 2 per semaphore		*/
+/*			2 for sleep list plus 2 per semaphore
+ *
+ * Lab 4 - Implementing blocked senders queue. Each process has its own queue,
+ *         so a head and a tail must be allocated for each process. Due to the
+ *         fact that a process can only be in one queue at a time, no further
+ *         changes are required.
+ *			*/
+
 #ifndef NQENT
-#define NQENT	(NPROC + 4 + NSEM + NSEM)
+#define NQENT	(NPROC + 4 + NSEM + NSEM + NPROC + NPROC)
 #endif
 
 #define	EMPTY	(-1)		/* Null value for qnext or qprev index	*/
