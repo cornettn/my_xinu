646df97095fc559338b31ff78a3e99ffeaf30593
Noah Liam Cornett <cornettn@xinu01.cs.purdue.edu>
2020-04-20 22:01:40 -0400

    Add more test cases

--- lab5/xinu-spring2020/system/main.c
+++ lab5/xinu-spring2020/system/main.c
@@ -5,7 +5,8 @@
 #define UNUSED(x) (void)(x)
 
 #define PART_3_CASE_I (0)
-#define PART_3_CASE_II (1)
+#define PART_3_CASE_II (0)
+#define PART_4 (1)
 
 pid32 recvpid;
 umsg32 ubuffer;
@@ -13,21 +14,30 @@ umsg32 ubuffer;
 void sender_proc() {
 
   /* Allow recv proc to register cb func */
-  sleepms(1000);
+  sleepms(500);
 
-  send(recvpid, 21);
+  send(recvpid, 69);
   XDEBUG_KPRINTF("sender_proc: msg sent\n");
+  send(recvpid, 70);
+  sleepms(1000);
+  send(recvpid, 71);
+  sleep(4);
+  send(recvpid, 72);
 }
 
 void recv_proc() {
 
-  long *cbvalid = (long *) &proctab[recvpid].prcbvalid;
+  struct procent *prptr;
+  prptr = &proctab[currpid];
+
+  long *cbvalid = (long *) &prptr->prcbvalid;
   long *tmpvalid = (long *) &proctab[recvpid].prtmpvalid;
   long *tmpbuf = (long *) &proctab[recvpid].prtmpbuf;
   long *mbuf = (long *) &proctab[recvpid].prmbufptr;
   long *ent = (long *) &proctab[recvpid];
   long *cbf = (long *) &proctab[recvpid].prcbptr;
   long *ubuf = (long *) &ubuffer;
+  long *state = (long *) &prptr->prstate;
 
   XDEBUG_KPRINTF("Address of proctab[currpid]: 0x%08x (%d)\n", ent, ent);
   XDEBUG_KPRINTF("Address of prcbvalid: 0x%08x (%d)\n", cbvalid, cbvalid);
@@ -36,6 +46,8 @@ void recv_proc() {
   XDEBUG_KPRINTF("Address of prmbufptr: 0x%08x (%d)\n", mbuf, mbuf);
   XDEBUG_KPRINTF("Address of prcbptr: 0x%08x (%d)\n", cbf, cbf);
   XDEBUG_KPRINTF("Address of ubuffer: 0x%08x (%d)\n", ubuf, ubuf);
+  XDEBUG_KPRINTF("Address of prstate: 0x%08x (%d)\n", state, state);
+  XDEBUG_KPRINTF("Address of sleep: 0x%08x (%d)\n", &sleep, &sleep);
 
   XDEBUG_KPRINTF("Offset for prcbvalid: %d\n", cbvalid - ent);
   XDEBUG_KPRINTF("Offset for prtmpvalid: %d\n", cbvalid - tmpvalid);
@@ -52,22 +64,27 @@ void recv_proc() {
   XDEBUG_KPRINTF("recv_proc: cb func registered\n");
 
   #if PART_3_CASE_I
-  sleepms(500);
+
+  XDEBUG_KPRINTF("---------------\n"
+                 "  CASE (I)     \n"
+                 "---------------\n");
+
+  sleep(1);
   XDEBUG_KPRINTF("recv_proc: out of sleep\n");
   XDEBUG_KPRINTF("weeeeeeee!\n");
-  #elif PART_3_CASE_II
+  sleep(1);
+  #endif
+
+  #if PART_3_CASE_II
 
   /* ensures that the time slice will be depleted */
 
-  XDEBUG_KPRINTF("recv_proc: starting loop\n");
-  uint32 counter = 0;
-  while(TRUE) {
-    counter++;
-    if (counter % 1000000 == 0) {
-      XDEBUG_KPRINTF("in recv_proc\n");
-      counter = 0;
-    }
-  }
+  XDEBUG_KPRINTF("---------------\n"
+                 "  CASE (II)    \n"
+                 "---------------\n"
+                 "recv_proc: starting loop\n");
+  while(TRUE)
+    ;
 
   #endif
 
@@ -75,7 +92,9 @@ void recv_proc() {
 
 process	main(void)
 {
-  XDEBUG_KPRINTF("main: starting\n");
+#if PART_3_CASE_I || PART_3_CASE_II
+
+  XDEBUG_KPRINTF("sizeof struct procent: %d\n", sizeof(struct procent));
   resume(create(sender_proc, 1024, 5, "sender", 0, NULL));
   recvpid = create(recv_proc, 1024, 5, "recv", 0, NULL);
   resume(recvpid);
@@ -86,9 +105,16 @@ process	main(void)
       &proctab[currpid], &proctab[currpid]);
   XDEBUG_KPRINTF("size of procent: %d\n", sizeof(struct procent));
 
-
-
-
   XDEBUG_KPRINTF("recv pid: %d\n", recvpid);
+#endif
+
+
+#if PART_4
+
+
+#endif
+
+
+
 
 }
